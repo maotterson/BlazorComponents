8f24b40f56fb7280ddbe1a0daa0c5c0982529b3c
maotterson <otterson.2@gmail.com>
2023-05-25 00:19:11 -0400

    refactor -> exercise service + inject into main razor component for sample component test

---
+++ BlazorComponents/Data/Services/ExerciseService.cs
@@ -0,0 +1,25 @@
+﻿using BlazorComponents.WASM.Data.Models;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace BlazorComponents.WASM.Data.Services;
+
+public class ExerciseService
+{
+    public List<Exercise> Exercises = new List<Exercise>();
+    private HttpClient _http;
+    public ExerciseService(HttpClient http)
+    {
+        _http = http;
+    }
+
+    public async Task Load()
+    {
+        var jsonContent = await _http.GetStringAsync("/sample-data/exercises.json");
+        var exercises = JsonSerializer.Deserialize<List<Exercise>>(jsonContent) ?? throw new Exception();
+        Exercises = exercises;
+        Exercises.ForEach(exercise => { 
+            Console.WriteLine(exercise.Name);
+        });
+    }
+}
