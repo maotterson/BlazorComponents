faa49884eb9f0df787db52606d6e78d7bd566c9b
maotterson <otterson.2@gmail.com>
2023-05-26 22:08:51 -0400

    create model to store a search result object

---
+++ BlazorComponents/Components/TypeaheadList/Models/ExerciseSearchResult.cs
@@ -0,0 +1,9 @@
+﻿using BlazorComponents.WASM.Data.Models;
+
+namespace BlazorComponents.WASM.Components.TypeaheadList.Models;
+
+public class ExerciseSearchResult
+{
+    public Exercise Exercise { get; set; } = default!;
+    public List<(int, int)> MatchIndices { get; set; } = default!;
+}
